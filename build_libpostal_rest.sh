ed5d812c4623f0d9633b45c3841e74e3c742b6ff
odmarkj <josh@isell.com>
2017-02-23 17:43:10 -0800

    Update reference to repo

--- build_libpostal_rest.sh
+++ build_libpostal_rest.sh
@@ -7,6 +7,6 @@ export GOROOT=/libpostal/go
 export GOPATH=/libpostal/workspace
 export PATH=$PATH:/libpostal/go/bin
 
-go get github.com/johnlonganecker/libpostal-rest
+go get github.com/creative-logic/libpostal-rest
 
-go install github.com/johnlonganecker/libpostal-rest
+go install github.com/creative-logic/libpostal-rest
